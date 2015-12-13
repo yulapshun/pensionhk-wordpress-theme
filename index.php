3226b76bcf100d4cd194d12834e6cad280251e36
yulapshun <yulapshun@gmail.com>
2015-12-13 23:10:38 +0800

    display slide show only on home

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 get_header();
-get_template_part('slide');
+if (is_home()) {
+  get_template_part('slide');
+}
 ?>
 <div id="primary" class="content-area">
   <main id="main" class="site-main" role="main">
